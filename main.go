3ed1d31aadf761b641a38c8235f5aa7bbd962682
Mina M. Labib <mina.tomas2000@gmail.com>
2018-04-16 16:54:26 +0200

    Update main.go
    - Adding json format
    - Adding xml format

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+	"encoding/xml"
+	"encoding/json"
 )
 
 type User struct {
@@ -20,17 +22,17 @@ type Post struct {
 }
 
 func main() {
-	//demo()
-	Postdemo()
+	demo()
+	//Postdemo()
 }
 func CreateUser(userdata User){
 
 }
 
-func CreatePost(userdata User, postdata Post){
-	// Putting the current time into the Post
-	postdate.CreatedAt = time.Now()
-}
+// func CreatePost(userdata User, postdata Post){
+// 	// Putting the current time into the Post
+// 	postdate.CreatedAt = time.Now()
+// }
 
 func CurrentLoggedUser(){
 
@@ -39,29 +41,36 @@ func Postdemo() {
 
 }
 
-// func demo() {
-// 	// Hello world page
-// 	fmt.Println("Hello world")
-// 	// Login/Registration
-// 	fmt.Println("(1) Login", "(2) Register")
-// 	var inputstr int
-// 	fmt.Scanf("%d", &inputstr)
-// 	if inputstr != 1 && inputstr != 2 {
-// 		fmt.Println("Enter valid input")
-// 	}
+func demo() {
+	// Hello world page
+	fmt.Println("Hello world")
+	// Login/Registration
+	fmt.Println("(1) Login", "(2) Register")
+	var inputstr int
+	fmt.Scanf("%d", &inputstr)
+	if inputstr != 1 && inputstr != 2 {
+		fmt.Println("Enter valid input")
+	}
 
-// 	if inputstr == 1 {
-// 		// Login page
-// 		var appuser User
-// 		fmt.Print("Enter email:")
-// 		fmt.Scanf("%s", appuser.Email)
-// 		fmt.Print("Enter password:")
-// 		fmt.Scanf("%s", appuser.Password)
-// 		// Verification
+	if inputstr == 1 {
+		// Login page
+		var appuser User
+		fmt.Print("Enter email:")
+		fmt.Scanf("%s", &appuser.Email)
+		fmt.Print("Enter password:")
+		fmt.Scanf("%s", &appuser.Password)
+		fmt.Print("Enter Name:")
+		fmt.Scanf("%s", &appuser.Name)
+		
+		xmlrespond, _ := xml.Marshal(appuser)
+		fmt.Println(string(xmlrespond))
+		jsonrespond, _ := xml.Marshal(appuser)
+		fmt.Println(string(jsonrespond))
+		// Verification
 
-// 	}
-// 	if inputstr == 2 {
-// 		// Reigstration page
+	}
+	if inputstr == 2 {
+		// Reigstration page
 
-// 	}
-// }
+	}
+}
