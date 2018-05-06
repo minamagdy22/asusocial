ee80174bd8f97e2fcbf935c1275c00b08cab1918
Mina Magdy <mina.tomas2000@gmail.com>
2018-05-06 20:43:24 +0200

    - adding version, authors and others to the cli-app - adding basic web starter message and cli starter message  On branch re-structure  Changes to be committed: 	modified:   main.go

--- main.go
+++ main.go
@@ -3,15 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
-
+	app.Name = "asu social"
+	app.Version = "19.99.0"
+	app.Compiled = time.Now()
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Mina Magdy",
+			Email: "mina.tomas2000@gmail.com",
+		},
+		cli.Author{
+			Name:  "Marco Younan",
+			Email: "xmarcoied@gmail.com",
+		},
+	}
+	app.Copyright = "(c) 2018 Serious Enterprise"
+	app.HelpName = "contrive"
+	app.Usage = "social network micro-application"
+	app.UsageText = "contrive - demonstrating the available API"
+	app.ArgsUsage = "[args and such]"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "port",
@@ -25,9 +45,10 @@ func main() {
 		case "main":
 			Welcome()
 		case "web":
-			fmt.Println("Hello from the web")
+			Welcome()
+			GoWeb()
 		case "cli":
-			fmt.Println("Hello from the cli")
+			GoCli()
 		default:
 			Welcome()
 			fmt.Println("Invalid port")
@@ -39,6 +60,25 @@ func main() {
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func GoWeb() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello from the web")
+
+	})
+	r.Run()
+}
+
+func GoCli() {
+	for true {
+		ClearScreen()
+		Welcome()
+		var command string
+		fmt.Print(">> ")
+		fmt.Scan(&command)
 	}
 }
 
@@ -61,6 +101,7 @@ func Welcome() {
 	fmt.Println(str)
 }
 
+//ClearScreen is cmd function to clean the screen
 func ClearScreen() {
 	cmd := exec.Command("clear") //Linux example, its tested
 	cmd.Stdout = os.Stdout
